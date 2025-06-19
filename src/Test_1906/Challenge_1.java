d32dfcf3b895c6c04745b7ba3cf8b9a46945f10f
Chaitali <chaitali.songire@gmail.com>
2025-06-19 08:05:06 +0530

    Challenge 1 test

---
+++ src/Test_1906/Challenge_1.java
@@ -0,0 +1,16 @@
+package Test_1906;
+
+public class Challenge_1 {
+    public static void main(String[] args) {
+        int Age = 25;               // int type
+        double pi = 3.14;             // double type
+        boolean isJavaFun = true;          // boolean type
+        char gradeLetter = 'A';            // char type
+
+        // Printing the variable values
+        System.out.println("Integer: " + Age);
+        System.out.println("Double: " + pi);
+        System.out.println("Boolean: " + isJavaFun);
+        System.out.println("Character: " + gradeLetter);
+    }
+}
